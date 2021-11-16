b165d46261184446fd69ce5b3b0c38dbad6ef33a
JackCS <jackfcs22@gmail.com>
2021-11-16 19:55:41 +0000

    Completed Debugging and Publishing sections

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 
-  console.log("it's working")
+  
+
   return (
     <View style={styles.container}>
       <Text>
