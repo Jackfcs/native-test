ad8a818938d5ad93a9ed8ba10fc5238b363635b4
JackCS <jackfcs22@gmail.com>
2021-11-17 18:24:51 +0000

    Added Alerts

--- App.js
+++ App.js
@@ -1,18 +1,77 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("pressed");
+  };
 
+  let value = 1;
+
+  const alertMe = () => {
+    console.log(value);
+
+    Alert.alert("Alert Title", "Here is the message", [
+      {
+        text: "yes",
+        onPress: () => {
+          console.log("yes");
+          value++;
+          console.log(value);
+        },
+      },
+      {
+        text: "No",
+        onPress: () => {
+          console.log("no");
+          value--;
+          console.log(value);
+        },
+      },
+    ]);
+  };
+
+  //iOS only
+  const promptMe = () => {
+    Alert.prompt("Prompt Title", "Prompt Message", text => console.log(text));
+  };
+    
   
 
   return (
     <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app and a test! How are you
-      </Text>
-      <StatusBar style="auto" />
+      <Button color="purple" title="Click Button" onPress={promptMe} />
+      <StatusBar style="dark" />
     </View>
+    // <View style={styles.container}>
+    //   <Text numberOfLines={1} onPress={handlePress}>
+    //     Open up App.js
+    //   </Text>
+    //   <TouchableOpacity>
+    //     <Image
+    //       source={{
+    //         width: 200,
+    //         height: 300,
+    //         uri: "https://picsum.photos/200/300",
+    //       }}
+    //     />
+    //   </TouchableOpacity>
+    //   <TouchableNativeFeedback >
+    //     <View style={{width: 200, height: 70, backgroundColor: "dodgerblue"}}>
+    //     </View>
+    //   </TouchableNativeFeedback>
+    //   <StatusBar style="dark" />
+    // </View>
   );
 }
 
@@ -20,9 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
-    color: "red",
-    textAlign: "center",
+    alignItems: "center",
   },
 });
