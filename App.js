28d4b9c1f10a2015d30b685501c50fccb927ffbb
JackCS <jackfcs22@gmail.com>
2021-11-24 21:42:57 +0000

    Native Flexbox

--- App.js
+++ App.js
@@ -14,64 +14,104 @@ import {
   SafeAreaView,
 } from "react-native";
 import Child from "./Child";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+//Native Hooks
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("pressed");
-  };
-
-  //Using device orientation with native hooks
-  const {landscape} = useDeviceOrientation();
-
-  let value = 1;
-
-  //Alert API
-  const alertMe = () => {
-    console.log(value);
-
-    Alert.alert("Alert Title", "Here is the message", [
-      {
-        text: "yes",
-        onPress: () => {
-          console.log("yes");
-          value++;
-          console.log(value);
-        },
-      },
-      {
-        text: "No",
-        onPress: () => {
-          console.log("no");
-          value--;
-          console.log(value);
-        },
-      },
-    ]);
-  };
-
-  //Prompts - iOS only
-  const promptMe = () => {
-    Alert.prompt("Prompt Title", "Prompt Message", (text) => console.log(text));
-  };
-
   return (
-    <View style={styles.container}>
-      <Button color="purple" title="Click Button" onPress={alertMe} />
-
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <View
-      style={{ backgroundColor: "dodgerblue", width: "100%", height: landscape ? "100%" : "30%" }}
-    ></View>
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 300,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 200,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
 
-const margin = { margin: 10 };
-const boxStyle = { width: 200, height: 70, backgroundColor: "dodgerblue" };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+// export default function App() {
+//   const handlePress = () => {
+//     console.log("pressed");
+//   };
+
+//   //Using device orientation with native hooks
+//   const {landscape} = useDeviceOrientation();
+
+//   let value = 1;
+
+//   //Alert API
+//   const alertMe = () => {
+//     console.log(value);
+
+//     Alert.alert("Alert Title", "Here is the message", [
+//       {
+//         text: "yes",
+//         onPress: () => {
+//           console.log("yes");
+//           value++;
+//           console.log(value);
+//         },
+//       },
+//       {
+//         text: "No",
+//         onPress: () => {
+//           console.log("no");
+//           value--;
+//           console.log(value);
+//         },
+//       },
+//     ]);
+//   };
+
+//   //Prompts - iOS only
+//   const promptMe = () => {
+//     Alert.prompt("Prompt Title", "Prompt Message", (text) => console.log(text));
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Button color="purple" title="Click Button" onPress={alertMe} />
+
+//       <View
+//       style={{ backgroundColor: "dodgerblue", width: "100%", height: landscape ? "100%" : "30%" }}
+//     ></View>
+//     </View>
+//   );
+// }
+
+// const margin = { margin: 10 };
+// const boxStyle = { width: 200, height: 70, backgroundColor: "dodgerblue" };
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//   },
+// });
