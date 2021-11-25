9d7e2d17e229ac96809fb72327911ec8972b2263
JackCS <jackfcs22@gmail.com>
2021-11-25 20:43:22 +0000

    Create home screen

--- App.js
+++ App.js
@@ -13,48 +13,56 @@ import {
   Platform,
   SafeAreaView,
 } from "react-native";
-import Child from "./Child";
 //Native Hooks
 import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
+    <WelcomeScreen />
+    
   );
 }
+
+// export default function App() {
+//   return (
+//     <View
+//       style={{
+//         backgroundColor: "#fff",
+//         flex: 1,
+//         flexDirection: "row",
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <View
+//         style={{
+//           backgroundColor: "dodgerblue",
+//           width: 100,
+//           height: 100,
+//         }}
+//       />
+//       <View
+//         style={{
+//           backgroundColor: "gold",
+//           width: 100,
+//           height: 100,
+//         }}
+//       />
+//       <View
+//         style={{
+//           backgroundColor: "tomato",
+//           width: 100,
+//           height: 100,
+//         }}
+//       />
+//     </View>
+//   );
+// }
+
 
 // export default function App() {
 //   const handlePress = () => {
