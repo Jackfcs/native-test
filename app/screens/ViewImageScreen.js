a068801ff4efcf1f0450b1c3e2174d472282c995
Jack Campbell-Smith <jack.campbell-smith@Z00006.local>
2021-12-08 15:09:47 +0000

    test

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         height: 50,
         backgroundColor: colors.primary,
         position: "absolute",
-        top: 30,
+        top: 40,
         left: 30
     },
     deleteIcon: {
